8f55fb4d91b2819b5e08de32c93d9dde6d351d23
Rashid Rashidov <rrashidov@gmail.com>
2022-09-02 22:53:50 +0300

    Implement mail notification client

--- internal/notification/notification.go
+++ internal/notification/notification.go
@@ -1,5 +1,37 @@
 package notification
 
+import "net/smtp"
+
 type NotificationClient interface {
 	Notify() error
+}
+
+type MailNotificationClient struct {
+	Host     string
+	Port     string
+	Mail     string
+	Password string
+}
+
+func (client MailNotificationClient) Notify() error {
+
+	from := client.Mail
+	password := client.Password
+
+	toEmailAddress := client.Mail
+	to := []string{toEmailAddress}
+
+	host := client.Host //"smtp.gmail.com"
+	port := client.Port //"587"
+	address := host + ":" + port
+
+	subject := "New article in usesthis.com"
+	body := "There is a new article in usesthis.com. Go check it out!"
+	message := []byte(subject + body)
+
+	auth := smtp.PlainAuth("", from, password, host)
+
+	err := smtp.SendMail(address, auth, from, to, message)
+
+	return err
 }
