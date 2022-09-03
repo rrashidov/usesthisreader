40733e207fc6831cb43c8094ddd48b8d91fc24fa
Rashid Rashidov <rrashidov@gmail.com>
2022-09-03 12:54:24 +0300

    AWS SES based notification client

--- internal/notification/notification.go
+++ internal/notification/notification.go
@@ -1,37 +1,104 @@
 package notification
 
-import "net/smtp"
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+)
+
+const (
+	// The subject line for the email.
+	Subject = "New article on usesthis.com"
+
+	// The HTML body for the email.
+	HtmlBody = "<h1>New article on usesthis.com. Go check it out!</h1>"
+
+	//The email body for recipients with non-HTML email clients.
+	TextBody = "New article on usesthis.com. Go check it out!"
+
+	// The character encoding for the email.
+	CharSet = "UTF-8"
+)
 
 type NotificationClient interface {
 	Notify() error
 }
 
-type MailNotificationClient struct {
-	Host     string
-	Port     string
-	Mail     string
-	Password string
+type AWSSESNotificationClient struct {
+	Region    string
+	Recipient string
+	Sender    string
 }
 
-func (client MailNotificationClient) Notify() error {
+func (client AWSSESNotificationClient) Notify() error {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(client.Region)},
+	)
 
-	from := client.Mail
-	password := client.Password
+	if err != nil {
+		return err
+	}
 
-	toEmailAddress := client.Mail
-	to := []string{toEmailAddress}
+	// Create an SES session.
+	svc := ses.New(sess)
 
-	host := client.Host //"smtp.gmail.com"
-	port := client.Port //"587"
-	address := host + ":" + port
+	// Assemble the email.
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(client.Recipient),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(HtmlBody),
+				},
+				Text: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(TextBody),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(CharSet),
+				Data:    aws.String(Subject),
+			},
+		},
+		Source: aws.String(client.Sender),
+	}
 
-	subject := "New article in usesthis.com"
-	body := "There is a new article in usesthis.com. Go check it out!"
-	message := []byte(subject + body)
+	// Attempt to send the email.
+	result, err := svc.SendEmail(input)
 
-	auth := smtp.PlainAuth("", from, password, host)
+	// Display error messages if they occur.
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
 
-	err := smtp.SendMail(address, auth, from, to, message)
+		return err
+	}
 
-	return err
+	fmt.Println("Email Sent to address: " + client.Recipient)
+	fmt.Println(result)
+
+	return nil
 }
