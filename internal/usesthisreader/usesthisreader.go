062618152262f6a0d021a5eeab73302fd89acb46
Rashid Rashidov <rrashidov@gmail.com>
2022-09-05 16:18:58 +0300

    Add logs to reader

--- internal/usesthisreader/usesthisreader.go
+++ internal/usesthisreader/usesthisreader.go
@@ -1,6 +1,9 @@
 package usesthisreader
 
-import "rrashidov/usesthisreader/internal/notification"
+import (
+	"fmt"
+	"rrashidov/usesthisreader/internal/notification"
+)
 
 type UsesThisReader interface {
 	Run() error
@@ -39,19 +42,31 @@ func (r SimpleUsesThisReader) Run() error {
 	latest_remote, err := r.remote.GetLatest()
 
 	if err != nil {
+		fmt.Printf("Problem retrieving remote latest interview: %q\n", err.Error())
+
 		return err
 	}
+
+	fmt.Printf("Latest remote interview: %q\n", latest_remote)
 
 	latest_local, err := r.local.GetLatest()
 
 	if err != nil {
+		fmt.Printf("Problem retrieving local latest interview: %q\n", latest_local)
+
 		return err
 	}
 
+	fmt.Printf("Latest local interview: %q\n", latest_local)
+
 	if latest_local != latest_remote {
+		fmt.Printf("New remote interview found. Notify")
+
 		r.local.Update(latest_remote)
 
 		return r.notif.Notify()
+	} else {
+		fmt.Printf("No new interview found")
 	}
 
 	return nil
